872e48c4ff691a607ebb8a673c37a3699f759e6e
aka-pnkz <ericfreire@live.com>
2025-12-09 08:45:44 -0300

    Updating

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,3 +1,14 @@
+
+import streamlit as st
+import pandas as pd
+
+from utils.storage import load_results
+from analysis.frequency import analyze_frequency, analyze_delay
+from analysis.generator import gerar_jogos
+from analysis.probability import prob_sena
+from pricing.pricing_table import preco_por_jogo, custo_total
+
+
 @st.cache_data
 def load_data():
     return load_results()
